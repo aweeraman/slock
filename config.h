a842204f08eb2a09da631032e9ffa331cebb7286
Anuradha Weeraman <aweeraman@gmail.com>
2020-08-10 21:32:07 -0400

    Keypress feedback patch

--- config.h
+++ config.h
@@ -4,9 +4,24 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INPUT] =  "black",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[BLOCKS] = "#005577",   /* key feedback block */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+// ### Blocks bar ###
+static short int blocks_enabled = 1; // 0 = don't show blocks
+static const int blocks_width = 0; // 0 = full width
+static const int blocks_height = 50;
+
+// position
+static const int blocks_x = 0;
+static const int blocks_y = 500;
+
+// Number of blocks
+static const int blocks_count = 20;
+// ### \Blocks bar ###
+
